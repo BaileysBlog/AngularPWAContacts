9c7bfd62b00f89a7c2c24e0fddef2b746699282b
Bailey Miller <bmiller@susquehannasoftware.com>
2018-09-10 07:00:01 -0400

    Deleted and re-made project

--- phoneBook/e2e/src/app.e2e-spec.ts
+++ phoneBook/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to phone-book!');
+    expect(page.getParagraphText()).toEqual('Welcome to phoneBook!');
   });
 });
