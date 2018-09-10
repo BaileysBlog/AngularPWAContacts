c649ef4b12ef7f23ece57c4625b79a51271c28e7
Bailey Miller <bmiller@susquehannasoftware.com>
2018-09-10 09:22:14 -0400

    Alright I think that is enough ui setup for one day

--- phoneBook/src/app/Components/contact-preview/contact-preview.component.ts
+++ phoneBook/src/app/Components/contact-preview/contact-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-preview',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-preview.component.css']
 })
 export class ContactPreviewComponent implements OnInit {
+
+  @Input() name: string;
 
   constructor() { }
 
--- phoneBook/src/app/Routes/contact-list/contact-list.component.ts
+++ phoneBook/src/app/Routes/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  public Contacts = [];
 
-  ngOnInit() {
+  constructor(private router: Router)
+  {
+    var ranNum = Math.floor(Math.random() * 50) + 1;
+    for (let index = 0; index < ranNum; index++)
+    {
+      this.Contacts.push({
+        Name: `${index}) Bailey Miller`,
+        Id: index
+      });
+    }
+   }
+
+  ngOnInit()
+  {
+    
+  }
+
+  NavigateToContactView(contactId: number)
+  { 
+    this.router.navigate(["Contacts", contactId]);
   }
 
 }
